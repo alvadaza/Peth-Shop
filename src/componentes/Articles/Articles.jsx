98f36a994d2b41683c12c1af1fa81dd7f2845d81
github_username <>
2025-12-03 17:40:58 -0500

    escroll

--- src/componentes/Articles/Articles.jsx
+++ src/componentes/Articles/Articles.jsx
@@ -1,13 +1,15 @@
 // src/componentes/Articulos.jsx
 import React from "react";
 import "./Articles.css";
+import useScrollAnimation from "../../hooks/useScrollAnimation";
 
 export default function Articulos({ articulos }) {
+  useScrollAnimation();
   return (
-    <section className="articulos-section">
+    <section className="articulos-section animate-on-scroll">
       <h3 className="articulos-title">Conoce Más Sobre Nosotros</h3>
 
-      <div className="articulos-grid">
+      <div className="articulos-grid animate-on-scroll">
         {articulos.map((art, index) => (
           <div className="articulo-card" key={index}>
             <img src={art.imagen} alt={art.titulo} />
